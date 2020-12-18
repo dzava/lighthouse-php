bcf6c055bb7afdf9eac105eb18edec2fd62c76f5
Dimitris Zavantias <dzavantias@gmail.com>
2020-12-18 19:27:05 +0200

    Remove unused code

--- tests/Integration/IntegrationTest.php
+++ tests/Integration/IntegrationTest.php
@@ -225,21 +225,4 @@ class IntegrationTest extends TestCase
 
         return $config;
     }
-
-    private function array_get($array, $key, $default = null)
-    {
-        if (is_null($key)) return $array;
-
-        if (isset($array[$key])) return $array[$key];
-
-        foreach (explode('.', $key) as $segment) {
-            if (!is_array($array) || !array_key_exists($segment, $array)) {
-                return $default;
-            }
-
-            $array = $array[$segment];
-        }
-
-        return $array;
-    }
 }
